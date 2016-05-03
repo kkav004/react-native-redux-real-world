7c9b4d7563529c52498f97404738a8e25a92c690
Kiryl Kavalenka <kiryl.kavalennka@gmail.com>
2016-05-03 21:21:24 +1200

    reorg

---
+++ old_src/index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import Kernel from './core';
+
+AppRegistry.registerComponent('App', () => Kernel);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import { AppRegistry } from 'react-native';
-import Kernel from './core';
+import React from 'react';
+import { Provider, connect } from 'react-redux';
+import { Router } from 'react-native-router-flux';
+
+import 'babel-polyfill'
+import { browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+import Root from './containers/Root'
+import configureStore from './store/configureStore'
+
+const store = configureStore()
+const history = syncHistoryWithStore(browserHistory, store)
+
+const RouterWithRedux = connect()(Router);
+const Kernel = () => (
+  <Provider  store={store} >
+     <Root history={history} />
+  </Provider>
+);
+
 
 AppRegistry.registerComponent('App', () => Kernel);
